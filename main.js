427f7f6fb828654c8f36062077a74b68e7ba5bab
Diogo <diogo.zfp@gmail.com>
2024-02-27 13:15:33 -0300

    Project puc

---
+++ main.js
@@ -0,0 +1,23 @@
+document.getElementById("segundaBtn").addEventListener("click", function() {
+    window.location.href = "segunda.html";
+});
+
+document.getElementById("tercaBtn").addEventListener("click", function() {
+    window.location.href = "terca.html";
+});
+
+document.getElementById("quartaBtn").addEventListener("click", function() {
+    window.location.href = "quarta.html";
+});
+
+document.getElementById("quintaBtn").addEventListener("click", function() {
+    window.location.href = "quinta.html";
+});
+
+document.getElementById("sextaBtn").addEventListener("click", function() {
+    window.location.href = "sexta.html";
+});
+
+document.getElementById("siteBtn").addEventListener("click", function() {
+    window.location.href = "https://sol.pucgoias.edu.br/aluno/";
+});
